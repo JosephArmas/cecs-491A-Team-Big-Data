fe1885f3e47ab78a4e008fee47a02d04cefba8ab
David DeGirolamo <daviddg5@yahoo.com>
2022-11-09 14:19:03 -0800

    now creates log

--- Source Code/TeamBigData.Utification/TeamBigData.Utification.Manager/Manager.cs
+++ Source Code/TeamBigData.Utification/TeamBigData.Utification.Manager/Manager.cs
@@ -17,12 +17,12 @@ namespace TeamBigData.Utification.ManagerLayer
             stopwatch.Start();
             response = accountManager.InsertUser("dbo.Users", email, password).Result;
             stopwatch.Stop();
-            if(stopwatch.ElapsedMilliseconds > 5000)
+            if(stopwatch.ElapsedMilliseconds > 5000 && response.isSuccessful)
             {
-                var sysUnderTest = new SqlDAO(@"Server=.;Database=TeamBigData.Utification.Logs;User=AppUser;Password=t;TrustServerCertificate=True;Encrypt=True");
-                var insertSql = "INSERT INTO dbo.Logs ([DateTime],LogLevel,Opr,Category,[Message]) VALUES ('" + DateTime.UtcNow.ToString() + "', 'Debug', 'DAO_MustLogWithin5Secs', 'Data','This is a automated test')";
-                var 
-
+                String username = response.errorMessage.Substring(47);
+                var logger = new SqlDAO(@"Server=.;Database=TeamBigData.Utification.Logs;User=AppUser;Password=t;TrustServerCertificate=True;Encrypt=True");
+                var insertSql = "INSERT INTO dbo.Logs ([DateTime],LogLevel,Opr,Category,[Message], User) VALUES ('" + DateTime.UtcNow.ToString() + "', 'Warning', 'Manager.InsertUser()', 'Data','Creating an a Account Took Longer than 5 Seconds', " + ")";
+                logger.Execute(insertSql);
             }
             return response;
         }
