844115646d318f8601ba71fddcad2ba53b36ba0b
David DeGirolamo <daviddg5@yahoo.com>
2023-02-19 21:15:58 -0800

    update sql instructions

--- SQL Instructions/UsersDDL.sql
+++
@@ -1,8 +0,0 @@
---TeamBigData.Utification.Users
-CREATE TABLE dbo.Users
-(
-username varchar(50),
-"password" varchar(200),
-"disabled" int DEFAULT 0,
-CONSTRAINT Users_Primary_Key PRIMARY KEY(username)
-);
