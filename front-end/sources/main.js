c7ac00af9c93722daf5175da6f32783bd4627d64
JosephArmas <armas.joseph98@gmail.com>
2023-02-26 02:23:02 -0800

    w:js to hide/show elements for registration and login | todo: home view, input validation for login/registration

---
+++ front-end/sources/main.js
@@ -0,0 +1,39 @@
+// Todo: 
+/*
+ * notify what a user their username when logged in
+ * check if a user is first logged in -> update-profile view
+ * input validations for login/registration?
+ * home view
+*/
+
+// reuse function to list on back button click
+document.querySelector(".back-button").addEventListener("click", homeClicked());
+// document.querySelector("#regBtn-submit").addEventListener("click", homeClicked());
+
+function loginClicked()
+{
+    var anonContainer = document.querySelector(".anon-container");
+    var loginContainer = document.querySelector(".login-container");
+    anonContainer.style.display = "none";
+    loginContainer.style.display = "block";
+}
+
+function homeClicked()
+{
+
+    var regContainer = document.querySelector(".registration-container");
+    var anonContainer = document.querySelector(".anon-container");
+    var loginContainer = document.querySelector(".login-container");
+    anonContainer.style.display = "block";
+    regContainer.style.display = "none";     
+    loginContainer.style.display = "none";
+
+}
+
+function regClicked()
+{
+    var regContainer = document.querySelector(".registration-container");
+    var anonContainer = document.querySelector(".anon-container");
+    anonContainer.style.display = "none";
+    regContainer.style.display = "block";
+}
