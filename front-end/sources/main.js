43bf6395e1ee648edaee5382edf27cdd1522f310
baexdot <baexdot@baexdots-MacBook-Pro.local>
2023-02-27 21:21:08 -0800

    w:working code to navigate from anon -> home | todo: hamburger menu js; ajax calls to backend; input validation;  error response

--- front-end/sources/main.js
+++ front-end/sources/main.js
@@ -3,13 +3,14 @@
  * notify what a user their username when logged in
  * check if a user is first logged in -> update-profile view
  * input validations for login/registration?
- * home view
- * do error screen
+ * home view - hamburger menu
+ * do error screen - show message validations & status codes
 */
 
 // reuse function to list on back button click
 document.querySelector(".back-button").addEventListener("click", homeClicked());
 // document.querySelector("#regBtn-submit").addEventListener("click", homeClicked());
+document.querySelector(".home-logoutBtn").addEventListener("click", homeClicked());
 
 function loginClicked()
 {
@@ -25,9 +26,12 @@ function homeClicked()
     var regContainer = document.querySelector(".registration-container");
     var anonContainer = document.querySelector(".anon-container");
     var loginContainer = document.querySelector(".login-container");
+    var homeContainer = document.querySelector(".home-container")
     anonContainer.style.display = "block";
+    homeContainer.style.display = "none";
     regContainer.style.display = "none";     
     loginContainer.style.display = "none";
+
 
 }
 
@@ -37,4 +41,25 @@ function regClicked()
     var anonContainer = document.querySelector(".anon-container");
     anonContainer.style.display = "none";
     regContainer.style.display = "block";
+}
+
+function regView()
+{
+    var homeContainer = document.querySelector(".home-container");
+    var anonContainer = document.querySelector(".anon-container");
+    var otpContainer =document.querySelector(".otp-container");
+    otpContainer.style.display = "none";
+    anonContainer.style.display = "none";
+    homeContainer.style.display = "block";
+}
+
+function otpView()
+{
+    var loginContainer = document.querySelector(".login-container");
+    var anonContainer = document.querySelector(".anon-container");
+    var otpContainer =document.querySelector(".otp-container");
+    loginContainer.style.display = "none";
+    anonContainer.style.display = "none";
+    otpContainer.style.display = "block";
+
 }
