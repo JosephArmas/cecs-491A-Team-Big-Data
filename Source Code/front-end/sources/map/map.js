3329a73fee5d931a225d4c75416f742c1bc97215
Ghabrille Ampo <75871616+ghabster01@users.noreply.github.com>
2023-03-08 12:28:19 -0800

    Working map.js and cypress e2e tests

--- Source Code/front-end/sources/map/map.js
+++ Source Code/front-end/sources/map/map.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// The cover is from Vatanak Vong's AJAX Demo in his GitHub
-// Google maps api code follows Google's map api doc tutorials
+// The cover is from Vatanak Vong's AJAX Demo in his GitHub. https://github.com/v-vong3/csulb/tree/master/cecs_491/demo/ajax-demo
+// Google maps api code follows Google's map api doc tutorials. https://developers.google.com/maps/documentation/javascript/overview#maps_map_simple-javascript
 (function (root, ajaxClient) {
     // Dependency check
     const isValid = root && ajaxClient;
@@ -19,22 +19,23 @@
     const CSULB = { lat: 33.7838, lng: -118.1141 };
 
     var script = document.createElement('script');
-    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAAfbLnE9etZVZ0_ZqaAPUMl03BfKLN8kI&callback=initMap';
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAAfbLnE9etZVZ0_ZqaAPUMl03BfKLN8kI&region=US&language=en&callback=initMap';
     script.async = true;
 
     let map;
 
-    const webServiceUrl = 'https://localhost:7079/account';
+    //const webServiceUrl = 'https://localhost:7079';
 
     window.getMarkerHandler = function () {
 
         // Connect to backend to get markers
-        var request = ajaxClient.get(webServiceUrl); // ('https://reqres.in/api/users?page=2');
+        /*var request = ajaxClient.get(webServiceUrl); // ('https://reqres.in/api/users?page=2');
         console.log(request);
         request.then(function (response) {
             console.log(response);
-        });
+        });*/
 
+        // Temp marker
         const marker = new google.maps.Marker({
             position: CSULB,
             map: map,
@@ -43,7 +44,7 @@
     }
    
     window.initMap = function() {
-        map = new google.maps.Map(document.getElementById("map"), {
+        map = new google.maps.Map(document.getElementById('map'), {
             center: CSULB,
             minZoom: 8,
             maxZoom: 18,
@@ -51,14 +52,13 @@
             mapId: "bb1d4678c71528ff",
             restriction: {
                 latLngBounds: CALIFORNIA_BOUNDS,
-                strictBounds: true,
-            },
+                strictBounds: false
+            }
         });
         getMarkerHandler();
     }
 
     document.head.appendChild(script);
-    initMap();
 
 })(window, window.ajaxClient);
 
