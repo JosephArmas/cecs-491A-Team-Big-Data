5e91c0376069b240bc500a5b8fbbff27140b1ffb
JosephArmas <armas.joseph98@gmail.com>
2023-03-10 01:53:41 -0800

    w:working checking the database before going to OTP view | todo: error codes; restrict OTP before going to home view

--- front-end/sources/login/login.js
+++ front-end/sources/login/login.js
@@ -1,30 +1,72 @@
+// * TODO: fix error validation 
 'use strict';
-// working dispaly data to div elementkj
-function displayData()
+const backendserver= 'https://localhost:7259/account/authentication';
+const loginForm = document.getElementById('login-form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const loginBtn = document.getElementById('sub-login');
+const errorsDiv = document.getElementById('errors');
+loginBtn.addEventListener('click', function (event)
 {
-    let testElement = document.getElementById('test-data');
-    axios.get('https://jsonplaceholder.typicode.com/posts').then( function (response) {
-        console.log(response.data)
-        testElement.innerHTML = JSON.stringify(response.data);
-    })
-    
-}
+    event.preventDefault();
+    if (email.value == '' || password.value == '')
+    {
+        errorsDiv.innerHTML = "Please fill in all fields";
+    }else {
 
-// displayData();
-function postDisplayData()
+    let user = {
+        username: email.value,
+        password: password.value
+    }
+    console.log(user.username);
+    console.log(user.password);
+    console.log(user);
+
+    axios.post(backendserver, user).then(function (response)
+    {
+        console.log(response.data);
+        var responseAfter = response.data
+        if(responseAfter.identity.isAuthenticated === true )
+        {
+            console.log(responseAfter.identity.isAuthenticated)
+            var loginContainer = document.querySelector(".login-container");
+            var otpContainer = document.querySelector(".otp-container");
+            otpContainer.style.display = "block";
+            loginContainer.style.display = "none";
+        }
+    })
+    }
+    // reset login form when button clicked
+    loginForm.reset()
+
+});
+
+// * Debugging Purposes
+/* 
+function displayLoginData()
 {
     let testElement = document.getElementById('test-data');
-    var dataa = 
-            {
-                "title": "foo",
-                "body": "bar",
-                "userId": 1
-          };
-    axios.post('https://jsonplaceholder.typicode.com/posts', dataa).then(function (response) {
-        console.log(response.data);
-        var dataAfter = response.data
+    let dataa = {
+        username:"testUser@yahoo.com",
+        password: "password"
+    }
+    // let headers = 
+    // {
+    //     "Access-Control-Allow-Origin": "*",
+    //     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    //     "Access-Control-Allow-Credentials":"true",
+    //     "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    // }
+
+    axios.post('https://localhost:7259/account/authentication',dataa) .then( function (response)
+    {
+        console.log(response.data)
+        var responseAfter = response.data
         testElement.innerHTML = JSON.stringify(response.data);
-        console.log(dataAfter.title);
+        // console.log(responseAfter._userID);
+        // console.log(responseAfter.identity.isAuthenticated);
+        // testElement.innerHTML = responseAfter._userID;
     })
 }
-postDisplayData();
+// displayLoginData();
+*/
