20f1aa1c5d9120a54a1b7045b9da504082f0940d
baexdot <baexdot@dhcp-39-39-188.dhcpint.csulb.edu>
2023-03-09 17:56:24 -0800

    w:post now works and getting properties from json data | todo: work on loging / registration

--- front-end/sources/login/login.js
+++ front-end/sources/login/login.js
@@ -4,9 +4,27 @@ function displayData()
 {
     let testElement = document.getElementById('test-data');
     axios.get('https://jsonplaceholder.typicode.com/posts').then( function (response) {
+        console.log(response.data)
         testElement.innerHTML = JSON.stringify(response.data);
     })
     
 }
 
 // displayData();
+function postDisplayData()
+{
+    let testElement = document.getElementById('test-data');
+    var dataa = 
+            {
+                "title": "foo",
+                "body": "bar",
+                "userId": 1
+          };
+    axios.post('https://jsonplaceholder.typicode.com/posts', dataa).then(function (response) {
+        console.log(response.data);
+        var dataAfter = response.data
+        testElement.innerHTML = JSON.stringify(response.data);
+        console.log(dataAfter.title);
+    })
+}
+postDisplayData();
