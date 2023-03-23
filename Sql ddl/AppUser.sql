67ebb1c940bb2558ae3d075712958be801067565
JosephArmas <armas.joseph98@gmail.com>
2023-03-23 00:25:52 -0700

    Cleaning...

--- Sql ddl/AppUser.sql
+++
@@ -1,4 +0,0 @@
--- this is for ONLY the TeamBigData.Utification.Logs
-CREATE USER AppUser FOR LOGIN AppUser
-
-GRANT INSERT to AppUser
--- Sql ddl/UserHash.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE UserHash
-(
-userHash varchar(200) NOT NULL,
-"userID" int NOT NULL,
-CONSTRAINT UserHash_Primary_Key PRIMARY KEY(userHash),
-);
