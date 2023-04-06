df1de6f8a2a4b254706e69daa9d8b4b656c70b7a
baexdot <baexdot@dhcp-39-106-235.dhcpint.csulb.edu>
2023-04-06 15:39:41 -0700

    Roles and end-point folder renamed and used obj.freeze

--- SourceCode/front-end/sources/roles/roles.js
+++ SourceCode/front-end/sources/roles/roles.js
@@ -1,10 +1,10 @@
 function getRole()
 {
-    const roles =  
+    const roles = Object.freeze 
     {
-        reg:['Regular User', 'Reputation User'],
-        service: ['Service User'],
-        admin: ['Admin User']
+        reg:['Regular User', 'Reputation User'];
+        service: ['Service User'];
+        admin: ['Admin User'];
     } 
 
     return roles
