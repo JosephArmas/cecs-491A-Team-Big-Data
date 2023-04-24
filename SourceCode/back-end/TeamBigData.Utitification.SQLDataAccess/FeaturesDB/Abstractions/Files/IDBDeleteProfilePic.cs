cc1ac5b334329293d8174e0b0a9d37a743642d47
David DeGirolamo <daviddg5@yahoo.com>
2023-04-24 16:59:58 -0700

    Revert "code update pt 2"
    This reverts commit d489ce86b406cbe1e33f8477742fcab08de852e6, reversing
    changes made to 4ea1ac04adc3695028deea0de7ad7c75d4fb43ca.

--- SourceCode/back-end/TeamBigData.Utitification.SQLDataAccess/FeaturesDB/Abstractions/Files/IDBDeleteProfilePic.cs
+++
@@ -1,9 +0,0 @@
-﻿using TeamBigData.Utification.ErrorResponse;
-
-namespace TeamBigData.Utification.SQLDataAccess.FeaturesDB.Abstractions.Files
-{
-    public interface IDBDeleteProfilePic
-    {
-        public Task<Response> DeleteProfilePic(int userID);
-    }
-}
--- SourceCode/back-end/TeamBigData.Utitification.SQLDataAccess/FeaturesDB/Abstractions/Pins/IDBSelectPinOwner.cs
+++
@@ -1,9 +0,0 @@
-﻿using TeamBigData.Utification.ErrorResponse;
-
-namespace TeamBigData.Utification.SQLDataAccess.FeaturesDB.Abstractions.Pins
-{
-    public interface IDBSelectPinOwner
-    {
-        public Task<Response> GetPinOwner(int pinID);
-    }
-}
