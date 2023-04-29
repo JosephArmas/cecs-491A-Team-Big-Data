5b391d29111de957f5c768bd54ab1be73d353cb5
baexdot <armas.joseph98@gmail.com>
2023-04-29 00:43:59 -0700

    cleaning...

--- dev/back-end/TeamBigData.Utification.SQLDataAccess/Abstractions/IDAO.cs
+++
@@ -1,9 +0,0 @@
-﻿using TeamBigData.Utification.ErrorResponse;
-
-namespace TeamBigData.Utification.SQLDataAccess
-{
-    public interface IDAO
-    {
-        Task<Response> Execute(object req);
-    }
-}
