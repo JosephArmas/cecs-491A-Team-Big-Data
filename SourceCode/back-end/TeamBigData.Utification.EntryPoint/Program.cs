103a5818e22b5a523ceaa35aa5f0ad2f0c70cfb9
David DeGirolamo <daviddg5@yahoo.com>
2023-05-07 14:40:24 -0700

    cors fix again

--- SourceCode/back-end/TeamBigData.Utification.EntryPoint/Program.cs
+++ SourceCode/back-end/TeamBigData.Utification.EntryPoint/Program.cs
@@ -132,7 +132,7 @@ app.Use(async (context, next) =>
     context.Response.OnStarting(() =>
     {
         context.Response.Headers.Add("access-control-allow-credentials", "true");
-        context.Response.Headers.Add("access-control-allow-headers", "content-type, authorization");
+        context.Response.Headers.Add("access-control-allow-headers", "content-type, authorization, ID");
         context.Response.Headers.Add("access-control-allow-methods", "GET, POST, OPTIONS");
         context.Response.Headers.Add("access-control-allow-origin", "*");
         context.Response.Headers.Add("access-control-max-age", "3600");
